65da5643a072d0ac0fcc8bdd6ea83fd29859a5d8
Valentin Dimitrov <v.dimitrov_1994@abv.bg>
2023-01-18 12:03:55 +0200

    'message'

---
+++ Home Task 6/Task6.js
@@ -0,0 +1,8 @@
+let input1 = prompt('Insert unput 1');
+let input2 = prompt('Insert input 2');
+
+let element1 = document.getElementById('input1')
+let element2 = document.getElementById('input2');
+
+element1.value= input1;
+element2.value = input2;
